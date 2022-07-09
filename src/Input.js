d0c6f27eb6a245f0ec2c1be22af851c5b4dcf751
dockomatik <doocko@gmail.com>
2022-07-09 23:34:04 +0200

    search bar update

--- src/Input.js
+++ src/Input.js
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
+import Data from "./MockData.json";
 
 const Input = () => {
-  const handleClick = (event) => {
-    console.log(event);
-  };
+  const [searchLetter, setSearchLetter] = useState("");
 
   return (
-    <div>
-      <input onChange={handleClick} className="input"></input>
+    <div className="input-div">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="input"
+        onChange={(event) => {
+          setSearchLetter(event.target.value);
+        }}
+      />
+      <div className="drop-input-active">
+        {Data.filter((val) => {
+          if (searchLetter == "") {
+            return val;
+          } else if (
+            val.first_name.toLowerCase().includes(searchLetter.toLowerCase())
+          ) {
+            return val;
+          }
+        }).map((val, key) => {
+          return (
+            <div className="input-drop-list" key={key}>
+              {val.first_name}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
